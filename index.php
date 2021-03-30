689a7b6de7045af4e84a25da8e176f4826847db3
Bunyamin Yigit <yigitbun@gmail.com>
2021-03-30 13:28:27 +0200

    data rendered

--- index.php
+++ index.php
@@ -9,21 +9,51 @@ $username = "byigit";
 $password = "hamm";
 $database = "bibliothek";
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+// Verbindung zur Datenbank herstellen
+$conn = mysqli_connect('localhost', 'byigit', 'hamm', 'bibliothek');
 
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-    // echo 'Erfolgreich verbunden';
-
-} catch (PDOException $e) {
-    echo "Verbindung fehlgeschlagen";
+// Verbindungsprüfung
+if (!$conn) {
+    echo "Verbindung fehlgeschlagen" . mysqli_connect_error();
 }
 
+// Anfrage für Bücher schreiben
+
+$sql = 'SELECT title, description FROM books';
+
+// eine Abfrage machen & Ergebnis erhalten
+
+$result = mysqli_query($conn, $sql);
+
+// die resultierenden Zeilen als Array holen
+
+$books = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+mysqli_free_result($result);
+
+mysqli_close($conn);
+
+
+
+// PDO Variante
+
+// try {
+//     $conn = new PDO("mysql:host=$host;dbname=$database", $username, $password);
+
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+//     // echo 'Erfolgreich verbunden';
+
+// } catch (PDOException $e) {
+//     echo "Verbindung fehlgeschlagen";
+// }
+
+
+
 // Error array erstellten
-
+$buchTitel = $desc = $verlag = '';
 $errors = array('buchTitel' => '', 'desc' => '', 'verlag' => '');
-
+$auswahl = '';
 // 'submit' Anfrage stellen
 
 if (isset($_POST['submit'])) {
@@ -43,8 +73,11 @@ if (isset($_POST['submit'])) {
             $errors['desc'] = 'Kurzbeschreibung darf nur aus Buchstaben und Leerzeichen bestehen!';
         }
     }
-    if (empty($_POST['verlag'])) {
-        $errors['verlag'] = 'Bitte einen Verlag auswählen!';
+    if (!empty($_POST['verlag'])) {
+        $selected = $_POST['verlag'];
+        $auswahl = 'Sie haben: ' . $selected . ' ausgewählt';
+    } else {
+        $errors['verlag'] = 'Sie haben keinen Verlag ausgewählt';
     }
 }
 
@@ -59,37 +92,40 @@ if (isset($_POST['submit'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Unsere Bibliothek</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/site.css">
 </head>
 
 <body>
+    <br>
+    <br>
     <div class="container">
         <h1>Unsere Bibliothek</h1>
         <form action="index.php" method="POST">
             <h3>Neues Buch anlegen</h3>
             <div class="mb-3">
                 <label for="buchTitel" class="form-label">Buchtitel</label>
-                <input type="text" class="form-control" id="buchTitel" name="buchTitel">
+                <input type="text" class="form-control" id="buchTitel" name="buchTitel" value="<?php echo $buchTitel ?>">
                 <div class="text-danger"><?php echo $errors['buchTitel']; ?></div>
             </div>
             <div class="mb-3">
                 <label for="desc" class="form-label">Kurzbeschreibung</label>
-                <textarea class="form-control" placeholder="Gebe hier eine kurze Beschreibung des Buches ein (max. 150 Zeichen)." id="desc" name="desc"></textarea>
+                <textarea class="form-control" placeholder="Gebe hier eine kurze Beschreibung des Buches ein (max. 150 Zeichen)." id="desc" name="desc" value="<?php echo $desc ?>"></textarea>
                 <div class="text-danger"><?php echo $errors['desc']; ?></div>
             </div>
             <div class="mb-3">
                 <label for="verlag" class="form-label">Verlag</label>
                 <select class="form-select" aria-label="Default select example" name="verlag">
-                    <option selected value="0">Verlag auswählen</option>
-                    <option value="1">One</option>
-                    <option value="2">Two</option>
-                    <option value="3">Three</option>
+                    <option disabled selected>Verlag auswählen</option>
+                    <option value="Verlag1">Verlag1</option>
+                    <option value="Verlag2">Verlag2</option>
+                    <option value="Verlag3">Verlag3</option>
                 </select>
                 <div class="text-danger"><?php echo $errors['verlag']; ?></div>
             </div>
 
 
             <div class="mb-3 form-check">
-                <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                <input type="checkbox" class="form-check-input" id="exampleCheck1" required>
                 <label class="form-check-label" for="exampleCheck1">Die in diesem Formular eingegebene Daten werden wervendet, um ein neues Buch in unserer Datenbank anzulegen. Die Daten sind durch Absenden des Formular für die Öffentlichkeit einsehbar.</label>
             </div>
             <button type="submit" class="btn btn-primary" name="submit" value="submit">Neues Buch erstellen</button>
@@ -102,6 +138,9 @@ if (isset($_POST['submit'])) {
     <div class="container">
         <h2>Unsere Bücher</h2>
 
+        <!-- 
+            // Beispiel Card
+            
         <div class="row">
             <div class="col-sm-3">
                 <div class="card" style="width: 18rem;">
@@ -112,14 +151,34 @@ if (isset($_POST['submit'])) {
                     </div>
                 </div>
             </div>
-        </div>
+        </div> -->
 
+        <div class="container">
+            <div class="row">
+                <?php foreach ($books as $book) { ?>
+                    <div class="col-sm-3">
+                        <div class="card" style="width: 18rem;">
+                            <div class="card-body">
+                                <h5 class="card-title"><?php echo htmlspecialchars($book['title']); ?></h5>
+                                <p class="card-text"><?php echo htmlspecialchars($book['description']); ?></p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+                <?php } ?>
+            </div>
+
+        </div>
     </div>
 
 
     <!-- Footer Bereich -->
-    <div class="container">
-    </div>
+    <br>
+    <br>
+    <br>
+    <div class="container text-center"><?php echo '&copy; Copyright ' . date('Y') . ' Yigit'; ?></div>
 </body>
 
 </html>
